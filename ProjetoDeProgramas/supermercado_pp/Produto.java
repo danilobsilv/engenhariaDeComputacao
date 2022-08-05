49f9569c03f01bad283a7a0c9fc785daeebadb59
Danilo Bruno da Silva <96261259+danilobsilv@users.noreply.github.com>
2022-08-04 20:35:13 -0400

    Create Produto.java

---
+++ ProjetoDeProgramas/supermercado_pp/Produto.java
@@ -0,0 +1,39 @@
+package supermercado_pp;
+
+public class Produto {
+    private String nome;
+    private float preco;
+    private int qtdeEstoque;
+
+    public Produto(String nome, float preco, int qtdeEstoque){
+        this.nome = nome;
+        this.preco = preco;
+        this.qtdeEstoque = qtdeEstoque;
+    }
+
+    public float getPreco() {
+        return preco;
+    }
+
+    public void setPreco(float preco) {
+        this.preco = preco;
+    }
+
+    public int getQtdeEstoque() {
+        return qtdeEstoque;
+    }
+
+    public void setQtdeEstoque(int qtdeEstoque) {
+        this.qtdeEstoque = qtdeEstoque;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+
+}
